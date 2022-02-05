5942733b37544497f5454327996f7e915060b90d
EderRoman <i2115665@continental.edu.pe>
2022-02-05 18:08:51 -0500

    My first file

---
+++ public_php/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+
+	<?php 
+		$n1=0;
+		$n2=1;
+		echo "Hello world";
+		if ($n1==0) {
+			echo "Es el mismo numero";
+		}
+		else{
+			echo "No es el mismo numero";
+		}
+
+
+
+
+
+
+
+
+
+	?>
+</body>
+</html>
